6dbe79198a2a549726d8e2d562c7cc4f8cc83990
Mariana Oliveira <olvr.mariana@gmail.com>
2020-03-17 01:28:48 -0300

    Implements Javascript in the slider

--- js/main.js
+++ js/main.js
@@ -2,10 +2,30 @@
 
 // menu dropdown
 function dropdownFunction() {
-    document.querySelector(".dropdown-content").classList.toggle("show");
+    document.getElementsByClassName("dropdown-content").classList.toggle("show");
   }
 
-  
-  
+
+// slider
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("slides");
+ 
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+  slides[slideIndex-1].style.display = "block";
+
+}
+
+
 
  
